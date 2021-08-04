0396432d0938dd9b57f57d8d78c516c83d2c21db
regtorks1 <reginald.torkonoo@amalitech.org>
2021-08-04 10:53:22 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CopyCatContainer from './containers/CopyCatContainer'
+
+ReactDOM.render(<CopyCatContainer />, document.getElementById('app'));
